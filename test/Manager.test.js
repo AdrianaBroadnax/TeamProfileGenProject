eed044095d6837ceedae2e9443d691aced1a43b7
Adriana Broadnax <alwaysadrianna@gmail.com>
2022-01-13 16:35:53 -0500

    test folder

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,1 +1,16 @@
-const Employee = require('../lib/Manager');
+const { TestWatcher } = require('jest');
+const Manager = require('../lib/Manager');
+
+
+test("get office number", ()=>{
+    const testNumber = "727"
+    const manager = new Manager("ally", 1, testNumber, "test@test.com");
+    expect(manager.getNumber()).toBe(testNumber)
+});
+
+
+test("get role", ()=>{
+    const testRole = "Manager"
+    const manager = new Manager("ally",1, "testtest.com", "testNumber");
+    expect(manager.getRole()).toBe(testRole)
+});
